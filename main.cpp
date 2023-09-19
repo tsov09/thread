9329018f99a3f43dac5e8ccb87e81fbdc7285825
tsov09 <tsovinarsafaryan147@gmail.com>
2023-09-19 19:45:17 +0400

    get n-th fibonacci number by separate thread

---
+++ main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <thread>
+#include <vector>
+#include <cstdlib> 
+#include <ctime> 
+
+int threads_count = 20;
+int rand_num_limit = 90;
+std::vector<std::thread> threads;
+std::vector<long long int> fibs(rand_num_limit, -1);
+
+
+long long int fib(long long int n) {
+	long long int res = 0;
+	if (fibs.at(n) != -1) {
+		return fibs[n];
+	}
+	else {
+		if (n < 0) {
+			res = 0;
+		}
+		else if (n <= 2) {
+			res = 1;
+		}
+		else {
+			res = fib(n - 1) + fib(n - 2);
+		}
+	}
+	fibs[n] = res;
+	return res;
+}
+
+
+int main() {
+	srand((unsigned int)time(NULL));
+	for (int i = 0; i < threads_count; i++) {
+		long long int res;
+		long long int n = rand() % rand_num_limit;
+		std::thread t([&res, n]() { 
+			res = fib(n); 
+			std::cout << res << std::endl;
+		});
+		threads.push_back(std::move(t));
+	}
+	for (int i = 0; i < threads_count; i++) {
+		threads[i].join();
+	}
+
+	return 0;
+}
